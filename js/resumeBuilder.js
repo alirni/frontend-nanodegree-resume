ae38d806a5a2f5fc3d15ca3060f7c0a3bb0c7d33
Ali Ramezani <aliramezani19.1@gmail.com>
2016-10-18 22:43:05 +0330

    add js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var email = "aliramezani19.1@gmail.com";
+var newEmail = email.replace("gmail", "mail");
+
+console.log(email);
+console.log(newEmail);
+
+var awesomeThoughts = "My name is ali and i am Awesome!!";
+var funThoughts = awesomeThoughts.replace("Awesome!!", "fuuuuun");
+
+console.log(funThoughts);
+
+$("#main").append(["funThoughts"]);
