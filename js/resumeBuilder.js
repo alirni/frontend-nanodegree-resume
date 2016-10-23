471530af04e40ab478d8ea0df14fc67d3aabba9f
Ali Ramezani <aliramezani19.1@gmail.com>
2016-10-23 20:53:17 +0330

    add code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -25,4 +25,31 @@ $("#header").prepend(formattedName);
 var myarray = ["ali", "JS", "Polymer", "web"];
 
 // $("#main").append(myarray);
-$("#main").append(myarray[2]);
+$("#main").append(myarray.length);
+
+var name = "AlbERt EINstEiN rnI";
+console.log("name : " + name);
+
+function nameChanger(oldName) {
+    console.log("oldName : " + oldName);
+    var finalName = oldName;
+    console.log("finalName : " + finalName);
+    // Your code goes here!
+    var names = oldName.split(" ");
+    console.log("names : " + names);
+    names[1] = names[1].toUpperCase();
+    names[2] = names[2].toLowerCase();
+    console.log("1- names : " + names);
+    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+    // names[0] = names[0].slice(2,3).toUpperCase();
+    console.log("2- names : " + names);
+    finalName = names.join(" ");
+    console.log("finalName : " + finalName);
+    
+    
+    // Don't delete this line!
+    return finalName;
+};
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
